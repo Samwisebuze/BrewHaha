5f1b411c261332e57c57477cb89f180b60c38d90
Samwisebuze <buzassg@mail.uc.edu>
2019-03-30 18:32:29 -0400

    WIP: Does it boot

--- start.js
+++ start.js
@@ -21,8 +21,3 @@ require('./models/User');
 // Start our app!
 const app = require('./app');
 
-app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
-    // eslint-disable-next-line no-console
-    console.log(`Express running → PORT ${server.address().port}`);
-});
