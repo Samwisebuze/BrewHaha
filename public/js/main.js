434baf52b79975df69d5b33417157dcd0e142511
Samwisebuze <buzassg@mail.uc.edu>
2019-03-31 10:07:25 -0400

    Get location data

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 /* eslint-env jquery, browser */
-import autocomplete from './modules/autocomplete';
-import { $, $$ } from './modules/bling';
+import autocomplete from './lib/autocomplete';
+import { $, $$ } from './lib/bling';
+
 $(document).ready(() => {
   autocomplete($('#address'), $('#lat'), $('#lng'));
 });
