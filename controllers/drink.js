9f43a0d7af54745e2006901ed3fa6cd529d8159e
Alex Vennemeyer <vennemeyera@gmail.com>
2019-03-31 02:37:21 -0400

    finished drink controller

--- controllers/drink.js
+++ controllers/drink.js
@@ -1,66 +1,65 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
-const Drink = mongoose.model('Drink');
 
+const Drink = mongoose.model('Drink');
 
 //== Create ==//
 // GET drink
 exports.addDrink = (req, res) => {
-    res.render('editDrink', {title: 'Add Drink'});
-}
+  res.render('editDrink', { title: 'Add Drink' });
+};
 
 // POST
- exports.postDrink = async (req, res) => {
-    const drink = await (new Drink(req.body)).save();
-    res.flash('sucess', `Added ${drink.name}`);
-    res.redirect(`/drinks/${drink.slug}`)
-
- }
+exports.postDrink = async (req, res) => {
+  const drink = await (new Drink(req.body)).save();
+  res.flash('sucess', `Added ${drink.name}`);
+  res.redirect(`/drinks/${drink.slug}`);
+};
 
 //== Edit ==//
 //GET
-exports.editDrink = async (req, res) {
-    const drink = await Drink.findOne({_id: req.params._id});
-    if (drink === null) {
-        res.flash('error','It appears this drink doesn\'t exist. Let\'s add it!');
-        res.redirect('/drinks/add')
-    }
-    res.render('drink', {title: `${drink.name}`, drink});
-}
+exports.editDrink = async (req, res) => {
+  const drink = await Drink.findOne({ _id: req.params._id });
+  if (drink === null) {
+    res.flash('error', 'It appears this drink doesn\'t exist. Let\'s add it!');
+    res.redirect('/drinks/add');
+  }
+  res.render('drink', { title: `${drink.name}`, drink });
+};
 // POST
-exports.updateDrink = async (req, res) {
-    const drink = await Drink.findOneAndUpdate({_id: req.params._id}, req.body, {
-        new: true,
-        runValidators: true
-    }).exec();
-    res.flash('sucess', `Updated ${drink.name}`);
-    req.redirect(`/drinks/${drink._id}/edit`);
-}
+exports.updateDrink = async (req, res) => {
+  const drink = await (new Drink(req.body)).save();
+  res.flash('sucess', `Updated ${drink.name}`);
+  req.redirect(`/drinks/${drink._id}/edit`);
+};
 
 //== read ==//
 
 //GET - one
-exports.getDrink = async (req, res) {
-    const drink = await Drink.findOne({_id: req.params._id});
-    if (drink == null) {
-        res.flash('error', 'It appears this drink does\'t exist. Let\'s add it!')
-    }
-}
+exports.getDrink = async (req, res) => {
+  const drink = await Drink.findOne({ _id: req.params._id });
+  if (drink == null) {
+    res.flash('error', 'It appears this drink does\'t exist. Let\'s add it!');
+  }
+  res.render('drink', { title: `${drink.name}`, drink });
+};
 
 // GET - list
-exports.getDrinks = async (req, res) {
-    const drinks = await Drink.find();
-    res.render('drinks', {title: 'Drinks', drinks})
-}
+exports.getDrinks = async (req, res) => {
+  const drinks = await Drink.find();
+  res.render('drinks', { title: 'Drinks', drinks });
+};
 
 // DELETE
-exports.deleteDrink = async (req, res) {
-    const drink = await Drink.findOneAndDelete({_id: req.params._id}).exec();
-    if (drink == null) {
-        res.flash('error','I\'m sorry Dave, I can\'t do that');
-        res.redirect(`/drinks/${req.params._id}/edit}`);
-    } 
-    res.flash('sucess', 'Drink deleted');
-    res.redirect('/drinks/');
-}
+exports.deleteDrink = async (req, res) => {
+  const drink = await Drink.findOneAndDelete({ _id: req.params._id }).exec();
+  if (drink == null) {
+    res.flash('error', 'I\'m sorry Dave, I can\'t do that');
+    res.redirect(`/drinks/${req.params._id}/edit}`);
+  }
+  res.flash('sucess', 'Drink deleted');
+  res.redirect('/drinks/');
+};
 
- // flash types: sucess, error, info, warning
+// flash types: sucess, error, info, warning
