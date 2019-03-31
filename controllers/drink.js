96bf1e83f0663ef7bf99821e2c4f3b4153dfceeb
Alex Vennemeyer <vennemeyera@gmail.com>
2019-03-31 00:06:36 -0400

    added read and delete to drink controller

--- controllers/drink.js
+++ controllers/drink.js
@@ -11,7 +11,7 @@ exports.addDrink = (req, res) => {
 // POST
  exports.postDrink = async (req, res) => {
     const drink = await (new Drink(req.body)).save();
-    req.flash('sucess', `Added ${drink.name}`);
+    res.flash('sucess', `Added ${drink.name}`);
     res.redirect(`/drinks/${drink.slug}`)
 
  }
@@ -21,7 +21,7 @@ exports.addDrink = (req, res) => {
 exports.editDrink = async (req, res) {
     const drink = await Drink.findOne({_id: req.params._id});
     if (drink === null) {
-        res.flash('error','It appears this drink doesn\'t exist. Let\'s make one!');
+        res.flash('error','It appears this drink doesn\'t exist. Let\'s add it!');
         res.redirect('/drinks/add')
     }
     res.render('drink', {title: `${drink.name}`, drink});
@@ -32,10 +32,35 @@ exports.updateDrink = async (req, res) {
         new: true,
         runValidators: true
     }).exec();
-    req.flash('sucess', `Updated ${drink.name}`);
+    res.flash('sucess', `Updated ${drink.name}`);
     req.redirect(`/drinks/${drink._id}/edit`);
 }
 
+//== read ==//
 
+//GET - one
+exports.getDrink = async (req, res) {
+    const drink = await Drink.findOne({_id: req.params._id});
+    if (drink == null) {
+        res.flash('error', 'It appears this drink does\'t exist. Let\'s add it!')
+    }
+}
 
- // flash types: sucess, failure, info, warning
+// GET - list
+exports.getDrinks = async (req, res) {
+    const drinks = await Drink.find();
+    res.render('drinks', {title: 'Drinks', drinks})
+}
+
+// DELETE
+exports.deleteDrink = async (req, res) {
+    const drink = await Drink.findOneAndDelete({_id: req.params._id}).exec();
+    if (drink == null) {
+        res.flash('error','I\'m sorry Dave, I can\'t do that');
+        res.redirect(`/drinks/${req.params._id}/edit}`);
+    } 
+    res.flash('sucess', 'Drink deleted');
+    res.redirect('/drinks/');
+}
+
+ // flash types: sucess, error, info, warning
